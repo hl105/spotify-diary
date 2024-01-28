47c4008d3fbd7e7998c1bb66be7722bd9f80ce49
Johanna <hl105@wellesley.edu>
2024-01-28 17:05:05 -0500

    index

--- javascript.js
+++ javascript.js
@@ -4,11 +4,12 @@ var trackNames = [];
 
 document.addEventListener("DOMContentLoaded", function() {
     path = window.location.pathname;
-    console.log(path);
     page = path.split("/").pop().split(".ht")[0];
+    /*
     if ((page!="index") && (page!="winter") && (page!="finals")){
         page = "index"; //default to index page
     }
+    */
 
     var imageTexts = document.querySelectorAll(".image-text");
 
